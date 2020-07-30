00e0255da0c18b4a0b8674eb0890638c799eb2fa
Peggy <peggy22509385@gmail.com>
2020-07-30 15:10:53 +0200

    154-167 project-routing

--- project/src/app/recipes/recipe-list/recipe.service.ts
+++ project/src/app/recipes/recipe-list/recipe.service.ts
@@ -25,6 +25,9 @@ export class RecipeService {
   getRecipes() {
     return this.recipes.slice();
   }
+  getRecipe(index:number) {
+    return this.recipes[index];
+  }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     console.log(ingredients);
